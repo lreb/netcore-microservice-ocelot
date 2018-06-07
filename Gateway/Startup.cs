9c20e63e92144fbe999086458b8db4b9f040596c
Luis Espinoza <luis.espinoza@buildbinder.com>
2018-06-07 14:38:03 -0600

    documented and added async to configuration method

--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -45,7 +45,7 @@ namespace Gateway
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             // if (env.IsDevelopment())
             // {
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # netcore-microservice-ocelot
+
+## Sources
+https://passos.com.au/ocelot/
+https://www.c-sharpcorner.com/article/building-api-gateway-using-ocelot-in-asp-net-core/
+https://www.c-sharpcorner.com/article/building-api-gateway-using-ocelot-in-asp-net-core-part-two/
+https://www.c-sharpcorner.com/article/building-api-gateway-using-ocelot-in-asp-net-core-part-three-logging2/
